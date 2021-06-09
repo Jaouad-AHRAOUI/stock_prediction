7ecef80065ba5185d0e08af32cf3164d598fbab0
Arnaud Raybaud <arnaud.raybaud@gmail.com>
2021-06-09 13:50:37 +0200

    Data Prep ready to use

--- stock_prediction/features_exo.py
+++ stock_prediction/features_exo.py
@@ -9,7 +9,7 @@ def exo_selection(df, sp500=True, gold=True, eurusd=True, nasdaq=True, crude=Tru
 
     # we want the path of where we rare
     we_are = os.getcwd()
-    path = we_are[:-16] + 'raw_data/'
+    path = we_are + '/raw_data/'
     # we need a list to store the column name  to be able to access easily when rebasing
     exo_col_name = []
     # we need to store the df in a list to know at the end which df we need to merge
@@ -23,6 +23,10 @@ def exo_selection(df, sp500=True, gold=True, eurusd=True, nasdaq=True, crude=Tru
         data_sp500 = pd.read_csv(path_of)
         # now we want the return of the Adj Close price
         data_sp500['Return_S&P500'] = data_sp500['Close/Last'].pct_change(1)
+        # the "Date" column does not have the same format
+        # we need to transform it
+        data_sp500['Date'] = data_sp500['Date'].map(
+            lambda x: f'{x[-4:]}-{x[:2]}-{x[3:5]}')
         # then as we only need the return (in case keep other features) we just keep this column and the Date
         data_sp500 = data_sp500[['Date', 'Return_S&P500']]
         # we store the name of the column
@@ -81,8 +85,9 @@ def exo_selection(df, sp500=True, gold=True, eurusd=True, nasdaq=True, crude=Tru
         data_vix = pd.read_csv(path_of)
         # for the VIX, index of volatility of US markets, we don't want thye return
         # we mill have to keep that index un-rebased
-        data_vix['Close'] = data_vix['Vix_No_Rebase']
+        data_vix['Vix_No_Rebase'] = data_vix['Close']
         data_vix = data_vix[['Date', 'Vix_No_Rebase']]
+        exo_df_list.append(data_vix)
 
     # now that we have all the df we need to merge them on Date to keep only one column
     # for date and one column for each index return
@@ -90,21 +95,17 @@ def exo_selection(df, sp500=True, gold=True, eurusd=True, nasdaq=True, crude=Tru
 
     # first we need to merge the first df of the list on the dataframe data_prep
     for dataframe in exo_df_list :
-        df = df.merge(dataframe, how='inner', on='Date')
+        df = df.merge(dataframe, how='left', on='Date')
+
+    # here we need to fill NaN to 0 for all Retuns columns
+    # but for vix, we need to value of the row -1
+    # vix_return column is not in the "exo_col_name" because no need to rebase later
+
+    dict_to_fill = {column: 0.0 for column in exo_col_name}
+    df.fillna(value=dict_to_fill, inplace=True)
+    df.fillna(method='ffill', inplace=True)
 
     # we now have the dataframe ready for the modelling
-    # this function returns the dataframe & the list of indexes we decided to include
-    # like that, when doing the rebasing, we will be able to rebase the selected indexes
-    # without naming them
-    return df, exo_col_name
+    # this function returns the dataframe
 
-def exo_selection_euro_stoxx_50(df, period,columns) :
-    '''This function will select the indexes we want to be part of the df'''
-    # we build the path of the euro stocks csv file
-    path = os.getcwd()[:-16] + 'raw_data/' + '^STOXX50E.csv'
-    df_es50 = pd.read_csv(path)
-    df['Return_euro_stoxx_50'] = df_es50['Close'].pct_change(1)
-    df['Period_Volum_euro_stoxx_50'] = df_es50['Volume'] / df_es50['Volume'].rolling(period).mean() - 1
-    for colomn in columns :
-        df[f'{colomn}_relatif'] = df[colomn] -df['Return_euro_stoxx_50']
     return df
