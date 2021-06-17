fab3c208da7af5c66be78cec1e862e17112d0018
Arnaud Raybaud <arnaud.raybaud@gmail.com>
2021-06-17 20:23:40 +0200

    final one

--- stock_prediction/tradding_app.py
+++ stock_prediction/tradding_app.py
@@ -70,6 +70,7 @@ def true_returns(start_date, end_date, dict_hard_data) :
     # so we make the search on EuroStoxx to know the gap
 
     # we create a empty dict to store the data
+    dict_open_prices = {}
     dict_close_prices = {}
     dict_true_returns = {}
 
@@ -90,17 +91,30 @@ def true_returns(start_date, end_date, dict_hard_data) :
         # we create the pct_change
         stock_df['Returns_true'] = stock_df['Adj Close'].pct_change(1)
         # don't forget that the first row is not part of our simulation
+
+        #**************************************************************
+        # for the rest of the code, all is named "CLOSE" , but making the control
+        # in the app trading, we cannot buy at the Close yesterday, knowing the
+        # close price , so we need to buy at the open price
+        # because no time until presentation of the project, we need to change this and
+        # keep the rest of the name on the code as CLOSE but it is the OPEN price
+        # in fact, I have more time now :) , train for tomorrow : let's try to complete the task
+        #**************************************************************
+
+        open_prices = stock_df[['Date', 'Open']].copy()
         close_prices = stock_df[['Date', 'Close']].copy()
         true_returns = stock_df[['Date', 'Returns_true']].copy()
         # we want to rename the columns to be able to keep the right stock
         # when merging
+        open_prices = open_prices.rename(columns={'Open': comp})
         close_prices = close_prices.rename(columns= {'Close' : comp})
         true_returns = true_returns.rename(columns={'Returns_true': comp})
         # we drop the first row
         # to do that we need to reset index
         true_returns.reset_index(drop=True, inplace=True)
-        true_returns.drop(0)
+        true_returns.drop(labels=0, axis='index')
         # we strore in the dictionaries
+        dict_open_prices[comp] = open_prices
         dict_close_prices[comp] = close_prices
         dict_true_returns[comp] = true_returns
 
@@ -113,21 +127,24 @@ def true_returns(start_date, end_date, dict_hard_data) :
     # the first df that is not for oters
     # or create a first column with the dates we expect to trade during the simulation
     # and merge on that and shift if there is missing value
+    df_open_prices = dict_open_prices[company_list[0]]
     df_close_prices = dict_close_prices[company_list[0]]
     df_true_returns = dict_true_returns[company_list[0]]
     for comp in company_list[1:] :
+        df_open_prices = df_open_prices.merge(dict_open_prices[comp], how='inner', on='Date')
         df_close_prices = df_close_prices.merge(dict_close_prices[comp], how='inner', on='Date')
         # we do the same for the returns true
         df_true_returns = df_true_returns.merge(dict_true_returns[comp], how='inner', on='Date')
     # we also need to change the index and put the date as indexes
+    df_open_prices = df_open_prices.set_index('Date')
     df_close_prices = df_close_prices.set_index('Date')
     df_true_returns = df_true_returns.set_index('Date')
 
     # now we have our  2 df
-    return df_close_prices, df_true_returns
+    return df_open_prices, df_close_prices, df_true_returns
 
 
-def portfolio(close_price, true_returns, best_true, best_pred, cash_invest, true=False) :
+def portfolio(open_price, close_price, true_returns, best_true, best_pred, cash_invest, true=False) :
     '''This function will use the df in inputs to compute the number of stocks we need to trade
     and impact the cash '''
 
@@ -192,26 +209,51 @@ def portfolio(close_price, true_returns, best_true, best_pred, cash_invest, true
 
             amount_to_trade = weight * dict_ext_variables['cash'][date_list[
                 position - 1]]
+
+            # here we need to make a condition
+            # because if the move between Close price day -1 and Open price of the day is higher
+            # than our prediction ---> our expected return is already done and we cannot make it anymore
+            # so we don't buy/ sell if it is the case
+            # the move overnight
+            overnight = (open_price.loc[date_list[position], stocks] / close_price.loc[date_list[position - 1], stocks]) -1
+
+            if abs(overnight) > abs(rets) :
+                nb_stocks = 0
+
             # number of shares we can buy/sell with this amount
-            nb_stocks = amount_to_trade // close_price.loc[date_list[position -1], stocks]
-            # the real amount invested in that stock
-            amount_traded = nb_stocks * close_price.loc[date_list[position -1], stocks]
+            nb_stocks = amount_to_trade // open_price.loc[date_list[position], stocks]
+            # the real amount invested in that stock TRADED ON THE OPEN PRICE OF THE DAY
+            amount_traded = nb_stocks * open_price.loc[date_list[position],
+                                                        stocks]
             # we need to store that amount to be able to apply to the final cash position at the end of the day
             amount_tot_invested += amount_traded
             # we need the amount at the end of the day regarding the price close at the end of the day
             amount_end_of_day = nb_stocks * close_price.loc[date_list[position], stocks]
             # we find the gains (losses???) at the end of the day, for that stock
             # there is a difference if we sell or buy the stock
-            perform_stock = (amount_end_of_day - amount_traded) * direction
-            perf_daily_saved += perform_stock
-            # we need the amount we would add if the return predicted was right
-            predicted_return_amount = amount_traded * rets * direction
+            # here we have to take in consideration the case where rets is >0 but the stock
+            # goes down , and contrary
+            perform_stock = (amount_end_of_day - amount_traded)
+            if perform_stock * rets > 0 :
+                perf_daily_saved += perform_stock
+            else :
+                perf_daily_saved += (perform_stock * -1)
+
+            # we need the amount we would had if the return predicted was right
+            # it takes in consideration the weight and the predicted return
+            predicted_return_amount = amount_to_trade * rets * direction
             perf_daily_pred += predicted_return_amount
             # we also need the true return for that stock that day
             rets_true = true_returns.loc[ptf_date, stocks]
+            # we also need the performance during the day
+            day_perf = (close_price.loc[date_list[position],
+                                       stocks] / open_price.loc[
+                                           date_list[position], stocks]) - 1
             # now we need to store the values in our dicts
-
-            dict_stocks[stocks][ptf_date] = [nb_stocks, rets, rets_true, predicted_return_amount, perform_stock]
+            dict_stocks[stocks][ptf_date] = [
+                nb_stocks, rets, rets_true, predicted_return_amount,
+                perform_stock, overnight, day_perf
+            ]
 
         # until now we made it for all the stocks in the portfolio for that day
         # before going to the next day, we need to store the global values for that day
